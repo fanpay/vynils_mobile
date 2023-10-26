803197d090d210012e413adbdc43265c34e5dacb
Fabián Payan <f.payan@uniandes.edu.co>
2023-10-25 22:35:20 -0500

    Carga inicial del proyecto

---
+++ app/src/main/java/com/uniandes/vynilsmobile/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.uniandes.vynilsmobile.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uniandes.vynilsmobile.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
