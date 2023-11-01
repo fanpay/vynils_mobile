4f730ead0faa2896f39b17abcab297607eabd72f
Sebastian Mascareno Dagnino <s.mascareno@uniandes.edu.co>
2023-10-31 21:08:08 -0700

    HU02 - Se finaliza el enlace a la vista detalle

--- app/src/main/java/com/uniandes/vynilsmobile/view/AlbumDetailFragment.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/view/AlbumDetailFragment.kt
@@ -6,18 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
 import com.uniandes.vynilsmobile.databinding.AlbumDetailFragmentBinding
 import com.uniandes.vynilsmobile.viewmodel.AlbumDetailViewModel
+
 
 class AlbumDetailFragment : Fragment() {
     private var _binding: AlbumDetailFragmentBinding? = null
     private val binding get() = _binding!!
 
-    val album = AlbumDetailFragmentArgs.fromBundle(requireArguments()).album
-
-    companion object {
-        fun newInstance() = AlbumDetailFragment()
-    }
 
     private lateinit var viewModel: AlbumDetailViewModel
 
@@ -33,7 +31,12 @@ class AlbumDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(AlbumDetailViewModel::class.java)
-        binding.tvDescription.text = album.description
+        val args: AlbumDetailFragmentArgs by navArgs()
+        binding.albumName.text = args.album.name
+        Picasso.get().load(args.album.cover).into(binding.albumImage)
+        binding.tvDescription.text = args.album.description
+        binding.tvReleaseDate.text = args.album.releaseDate
+        binding.tvGenre.text = args.album.genre
     }
 
     override fun onDestroyView() {
