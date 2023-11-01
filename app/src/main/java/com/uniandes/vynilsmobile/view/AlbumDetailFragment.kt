3adb5c5abdddd8667cc275442ac2392a9d10aaaa
Sebastian Mascareño Dagnino <s.mascareno@uniandes.edu.co>
2023-10-31 21:49:34 -0700

    Feature/hu02 consultar informacion detallada de un album (#51)
    * [HU01] Se agrega implementacion para obtener albumes de api remota
    
    * [HU01] Se adiciona mejor manejo para errores de red. Se agrega prueba inicial con Espresso
    
    * [HU01]: Se arregla URL de pruebas.
    
    * [HU01]: Se incorpora menú de navegación inferior.
    
    * [HU01] Se adiciona componente que visualiza un error para utilizar en todos los fragmentos de la aplicación.
    
    * [HU01]: Se actualiza minSDK para permitir ejecución de la aplicación en Android L. Se reemplaza librería Glide por Picasso.
    
    * Avances en el layout del detail
    
    * Test enviando objeto album a detalle
    
    * HU02 - Se finaliza el enlace a la vista detalle
    
    * Actualización de la carga de la imagen en el detalle
    
    Co-Authored-By: Sebastian Mascareño Dagnino <123704723+sebastianmd11@users.noreply.github.com>
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    Co-Authored-By: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
    
    * Probando el boton para retroceder
    
    * Se actualiza tamaño de flecha de navegación
    
    Co-Authored-By: Sebastian Mascareño Dagnino <123704723+sebastianmd11@users.noreply.github.com>
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    Co-Authored-By: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
    
    ---------
    
    Co-authored-by: Fabián Payan <f.payan@uniandes.edu.co>
    Co-authored-by: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    Co-authored-by: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>

--- app/src/main/java/com/uniandes/vynilsmobile/view/AlbumDetailFragment.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/view/AlbumDetailFragment.kt
@@ -6,28 +6,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
 import com.uniandes.vynilsmobile.R
+import com.uniandes.vynilsmobile.databinding.AlbumDetailFragmentBinding
 import com.uniandes.vynilsmobile.viewmodel.AlbumDetailViewModel
 
-class AlbumDetailFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AlbumDetailFragment()
-    }
+class AlbumDetailFragment : Fragment() {
+    private var _binding: AlbumDetailFragmentBinding? = null
+    private val binding get() = _binding!!
+
 
     private lateinit var viewModel: AlbumDetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.album_detail_fragment, container, false)
+    ): View {
+        _binding = AlbumDetailFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(AlbumDetailViewModel::class.java)
-        // TODO: Use the ViewModel
+        val args: AlbumDetailFragmentArgs by navArgs()
+        binding.albumName.text = args.album.name
+        Picasso.get()
+            .load(args.album.cover)
+            .placeholder(R.drawable.ic_baseline_album_24)
+            .error(R.drawable.ic_baseline_android_24)
+            .into(binding.albumImage)
+
+        binding.tvDescription.text = args.album.description
+        binding.tvReleaseDate.text = args.album.releaseDate
+        binding.tvGenre.text = args.album.genre
+        binding.btBack.setOnClickListener{requireActivity().onBackPressed()}
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
