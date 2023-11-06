fcd512bc0bd55c79ebb713f92cfae19b776a782d
Fabián Payan <f.payan@uniandes.edu.co>
2023-11-06 07:14:27 -0500

    [Release]Se realiza merge de la rama release con la versión 1.0.0 a develop (#57)
    * Se crea nueva versión v1.0.0
    
    * Se agrega especificación de las pruebas E2E
    
    ---------
    
    Co-authored-by: Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>

--- app/src/main/java/com/uniandes/vynilsmobile/view/CommentFragment.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/view/CommentFragment.kt
@@ -41,38 +41,8 @@ class CommentFragment : Fragment() {
         recyclerView.adapter = viewModelAdapter
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        /*val activity = requireNotNull(this.activity) {
-            "You can only access the viewModel after onActivityCreated()"
-        }
-        activity.actionBar?.title = getString(R.string.title_comments)
-        val args: CommentFragmentArgs by navArgs()
-        Log.d("Args", args.albumId.toString())
-        viewModel = ViewModelProvider(this, CommentViewModel.Factory(activity.application, args.albumId)).get(CommentViewModel::class.java)
-        viewModel.comments.observe(viewLifecycleOwner, Observer<List<Comment>> {
-            it.apply {
-                viewModelAdapter!!.comments = this
-                if(this.isEmpty()){
-                    binding.txtNoComments.visibility = View.VISIBLE
-                }else{
-                    binding.txtNoComments.visibility = View.GONE
-                }
-            }
-        })*/
-        /*viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
-            if (isNetworkError) onNetworkError()
-        })*/
-    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-    /*private fun onNetworkError() {
-        if(!viewModel.isNetworkErrorShown.value!!) {
-            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
-            viewModel.onNetworkErrorShown()
-        }
-    }*/
 }
