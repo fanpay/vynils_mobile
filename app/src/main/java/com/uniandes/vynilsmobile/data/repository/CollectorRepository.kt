0aab11d08b30553fac88da138a2e605fee45c603
Sebastian Mascareño Dagnino <s.mascareno@uniandes.edu.co>
2023-11-16 00:05:36 -0700

    Implementando Corrutinas en llamado a API para colectores
    Co-Authored-By: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>

--- app/src/main/java/com/uniandes/vynilsmobile/data/repository/CollectorRepository.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/data/repository/CollectorRepository.kt
@@ -5,18 +5,27 @@ import com.uniandes.vynilsmobile.R
 import com.uniandes.vynilsmobile.data.exceptions.ApiRequestException
 import com.uniandes.vynilsmobile.data.model.Collector
 import com.uniandes.vynilsmobile.data.service.RetrofitBroker
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class CollectorRepository(val application: Application) {
     suspend fun getAllCollectors(): List<Collector> {
-        return try {
-            var collectors: List<Collector> = emptyList()
-            RetrofitBroker.getCollectors(
-                onComplete = { response -> collectors = response },
-                onError = { error -> throw ApiRequestException(application.resources.getString(R.string.error_retrieve_collectors), error) }
-            )
-            collectors
-        } catch (e: Throwable) {
-            throw ApiRequestException("${R.string.error_retrieve_collectors} -> ${e.cause}", e)
+        return withContext(Dispatchers.IO) {
+            try {
+                var collectors: List<Collector> = emptyList()
+                RetrofitBroker.getCollectors(
+                    onComplete = { response -> collectors = response },
+                    onError = { error ->
+                        throw ApiRequestException(
+                            application.resources.getString(R.string.error_retrieve_collectors),
+                            error
+                        )
+                    }
+                )
+                return@withContext collectors
+            } catch (e: Throwable) {
+                throw ApiRequestException("${R.string.error_retrieve_collectors} -> ${e.cause}", e)
+            }
         }
     }
     suspend fun createCollector(collector: Collector) = RetrofitBroker.createCollector(collector)
