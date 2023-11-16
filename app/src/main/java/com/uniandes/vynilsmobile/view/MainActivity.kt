77c8adb44a825368edb71ad61de02aca085efd57
Sebastian Mascareño Dagnino <s.mascareno@uniandes.edu.co>
2023-11-15 19:56:34 -0700

    Se elimina la inicialización de picaso en main activity para corregir falla en pruebas
    Co-Authored-By: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>

--- app/src/main/java/com/uniandes/vynilsmobile/view/MainActivity.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/view/MainActivity.kt
@@ -19,19 +19,8 @@ import com.uniandes.vynilsmobile.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
-    private fun initPicasso(){
-        val picasso = Picasso.Builder(this)
-            .listener { _, _, exception -> exception.printStackTrace() }
-            .indicatorsEnabled(false)
-
-        val built = picasso.build()
-        Picasso.setSingletonInstance(built)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        initPicasso()
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
