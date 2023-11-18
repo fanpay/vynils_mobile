5b913cc5b932f7e735b141e0055427a651946a4d
Sebastian Mascareño Dagnino <s.mascareno@uniandes.edu.co>
2023-11-17 18:35:07 -0700

    Se añade entidad de Artistas a VinylsDatabase
    Co-Authored-By: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>

--- app/src/main/java/com/uniandes/vynilsmobile/data/database/VinylsDatabase.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/data/database/VinylsDatabase.kt
@@ -5,12 +5,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.uniandes.vynilsmobile.data.model.Album
+import com.uniandes.vynilsmobile.data.model.Artist
 import com.uniandes.vynilsmobile.data.model.Collector
 import com.uniandes.vynilsmobile.data.model.Comment
 
 
 @Database(
-    entities = [Album::class, Collector::class, Comment::class],
+    entities = [Album::class, Artist::class, Collector::class, Comment::class],
     version = 1,
     exportSchema = false
 )
