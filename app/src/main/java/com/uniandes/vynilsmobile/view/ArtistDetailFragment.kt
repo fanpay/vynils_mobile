cf87f543ba9189fe082e51b1bb0ee5d085b92a0f
Fabián Payan <f.payan@uniandes.edu.co>
2023-11-19 08:35:20 -0500

    Se elimina import no utilizado
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    Co-Authored-By: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>

--- app/src/main/java/com/uniandes/vynilsmobile/view/ArtistDetailFragment.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/view/ArtistDetailFragment.kt
@@ -1,15 +1,14 @@
 package com.uniandes.vynilsmobile.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.squareup.picasso.Picasso
 import com.uniandes.vynilsmobile.R
-import com.uniandes.vynilsmobile.data.model.Artist
 import com.uniandes.vynilsmobile.databinding.ArtistDetailFragmentBinding
 
 class ArtistDetailFragment : Fragment() {
