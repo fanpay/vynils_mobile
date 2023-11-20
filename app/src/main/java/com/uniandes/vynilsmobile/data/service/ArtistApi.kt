3b1a4f8e2521bf8ce932bac9e97a9c60b5c31c8d
Fabián Payan <f.payan@uniandes.edu.co>
2023-11-19 19:30:23 -0500

    Se realiza corrección de URL para BackEnd
    - Se realiza corrección de URL para BackEnd
    - Actualización README
    - Actualización APK

--- app/src/main/java/com/uniandes/vynilsmobile/data/service/ArtistApi.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/data/service/ArtistApi.kt
@@ -2,16 +2,11 @@ package com.uniandes.vynilsmobile.data.service
 
 import com.uniandes.vynilsmobile.data.model.Artist
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
 
 interface ArtistApi {
 
     @GET("musicians")
     suspend fun getAllArtists(): Response<List<Artist>>
-
-    @POST("musicians")
-    suspend fun createArtist(@Body album: Artist) : Response<Artist>
 
 }
--- app/src/main/java/com/uniandes/vynilsmobile/data/service/RetrofitBroker.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/data/service/RetrofitBroker.kt
@@ -1,11 +1,8 @@
 package com.uniandes.vynilsmobile.data.service
 
-import android.util.Log
 import com.uniandes.vynilsmobile.data.model.Album
 import com.uniandes.vynilsmobile.data.model.Artist
-import com.uniandes.vynilsmobile.data.model.Band
 import com.uniandes.vynilsmobile.data.model.Collector
-import com.uniandes.vynilsmobile.data.model.Comment
 
 class RetrofitBroker {
 
@@ -28,45 +25,6 @@ class RetrofitBroker {
             }
         }
 
-        suspend fun createAlbum(album: Album): Album? {
-            val request = ApiClient.albums.createAlbum(album)
-            return if (request.isSuccessful){
-                Log.e("SuccessCrearAlbum", request.toString())
-                request.body()
-            }else{
-                Log.e("ErrorCrearAlbum", request.toString())
-                null
-            }
-        }
-
-        // Bands
-        suspend fun getBands(
-            onComplete: (resp: List<Band>) -> Unit,
-            onError: (error: Throwable) -> Unit
-        ) {
-            try {
-                val response = ApiClient.bands.getAllBands()
-                if (response.isSuccessful) {
-                    onComplete(response.body() ?: emptyList())
-                } else {
-                    onError(Exception("Error en la solicitud a la API: ${response.code()}"))
-                }
-            } catch (e: Throwable) {
-                onError(e)
-            }
-        }
-
-        suspend fun createBand(band: Band): Band? {
-            val request = ApiClient.bands.createBand(band)
-            return if (request.isSuccessful) {
-                Log.e("SuccessCrearBand", request.toString())
-                request.body()
-            } else {
-                Log.e("ErrorCrearBand", request.toString())
-                null
-            }
-        }
-
         // Artists
         suspend fun getArtists(
             onComplete: (resp: List<Artist>) -> Unit,
@@ -81,17 +39,6 @@ class RetrofitBroker {
                 }
             } catch (e: Throwable) {
                 onError(e)
-            }
-        }
-
-        suspend fun createArtist(artist: Artist): Artist? {
-            val request = ApiClient.artists.createArtist(artist)
-            return if (request.isSuccessful) {
-                Log.e("SuccessCrearArtist", request.toString())
-                request.body()
-            } else {
-                Log.e("ErrorCrearArtist", request.toString())
-                null
             }
         }
 
@@ -111,22 +58,6 @@ class RetrofitBroker {
                 onError(e)
             }
         }
-
-        suspend fun createCollector(collector: Collector): Collector? {
-            val request = ApiClient.collectors.createCollector(collector)
-            return if (request.isSuccessful) {
-                Log.e("SuccessCrearCollector", request.toString())
-                request.body()
-            } else {
-                Log.e("ErrorCrearCollector", request.toString())
-                null
-            }
-        }
-
-        fun getAllComments (): List<Comment> {
-            return listOf()
-        }
-
     }
 
 }
