bc9c3b84cc58ea29eb78c11523adfbec1785fea5
Fabián Payan <f.payan@uniandes.edu.co>
2023-11-23 17:12:47 -0500

    [HU03] Consultar listado artistas más corrutinas room (#77)
    * Avance con error de albums table en BD
    
    Co-Authored-By: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    
    * Se añade entidad de Artistas a VinylsDatabase
    
    Co-Authored-By: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
    Co-Authored-By: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    
    * Se realiza merge de la rama develop
    
    * Se eliminan problemas de rendimiento e inicialización de clases DAO
    
    Co-Authored-By: Sebastian Mascareño Dagnino <123704723+sebastianmd11@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sebastian Mascareño Dagnino <s.mascareno@uniandes.edu.co>
    Co-authored-by: Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
    Co-authored-by: Johana Ojeda <j.ojedaa@uniandes.edu.co>
    Co-authored-by: Sebastian Mascareño Dagnino <123704723+sebastianmd11@users.noreply.github.com>

--- app/src/main/java/com/uniandes/vynilsmobile/view/CollectorDetailFragment.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/view/CollectorDetailFragment.kt
@@ -1,15 +1,13 @@
 package com.uniandes.vynilsmobile.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.squareup.picasso.Picasso
 import com.uniandes.vynilsmobile.R
-import com.uniandes.vynilsmobile.databinding.AlbumDetailFragmentBinding
 import com.uniandes.vynilsmobile.databinding.CollectorDetailFragmentBinding
 
 class CollectorDetailFragment : Fragment() {
--- app/src/main/java/com/uniandes/vynilsmobile/viewmodel/ArtistViewModel.kt
+++ app/src/main/java/com/uniandes/vynilsmobile/viewmodel/ArtistViewModel.kt
@@ -10,7 +10,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.uniandes.vynilsmobile.data.model.Artist
 import com.uniandes.vynilsmobile.data.repository.ArtistRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class ArtistViewModel(application: Application) :  AndroidViewModel(application) {
@@ -32,31 +34,60 @@ class ArtistViewModel(application: Application) :  AndroidViewModel(application)
     val isNetworkErrorShown: LiveData<Boolean>
         get() = _isNetworkErrorShown
 
+    private var _eventNotDataFound = MutableLiveData(false)
+
+    val eventNotDataFound: LiveData<Boolean>
+        get() = _eventNotDataFound
+
+    private var _isNotDataFoundShown = MutableLiveData(false)
+
+    val isNotDataFoundShown: LiveData<Boolean>
+        get() = _isNotDataFoundShown
+
     init {
         artistsRepository = ArtistRepository(application)
         refreshDataFromNetwork()
     }
 
     private fun refreshDataFromNetwork() {
-        viewModelScope.launch {
-            try {
-                _artists.value = artistsRepository.getAllArtists()
+        viewModelScope.launch (Dispatchers.IO){
+            try{
+                withContext(Dispatchers.Main){
+                    val data = artistsRepository.getAllArtists()
+                    _artists.value = data
+
+                    if(data.isEmpty()){
+                        _eventNotDataFound.postValue(true)
+                        _isNotDataFoundShown.postValue(true)
+                    }
+                    else{
+                        _eventNotDataFound.postValue(false)
+                        _isNotDataFoundShown.postValue(false)
+                    }
+                }
                 _eventNetworkError.postValue(false)
                 _isNetworkErrorShown.postValue(false)
             }
-            catch(e:Exception) {
+            catch (e:Exception){
                 Log.e("refreshDataFromNetwork", e.toString())
-                _eventNetworkError.value = true
+                _eventNetworkError.postValue(true)
+
+                _eventNotDataFound.postValue(false)
+                _isNotDataFoundShown.postValue(false)
+
             }
+
         }
     }
-
     fun onNetworkErrorShown() {
         _isNetworkErrorShown.value = true
     }
+    fun onNotDataFoundShown() {
+        _isNotDataFoundShown.value = true
+    }
 
     class Factory(val app: Application) : ViewModelProvider.Factory {
-         override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(ArtistViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
                 return ArtistViewModel(app) as T
